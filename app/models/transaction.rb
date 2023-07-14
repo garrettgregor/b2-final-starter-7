f339d97a9bb9e4524fcf92b77b30d78b7b87eef9
Garrett Gregor <garrett.gregor@gmail.com>
2023-07-14 15:25:40 -0600

    add scope to transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,11 @@ class Transaction < ApplicationRecord
   validates_presence_of :invoice_id,
                         :credit_card_number,
                         :result
+
+  scope :successful, -> { where(result: "success") }
+
+  default_scope { order(:id) }
+
   enum result: [:failed, :success]
 
   belongs_to :invoice
